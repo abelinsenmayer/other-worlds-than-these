a240111b408cef160d2bb18574ca385f497786b0
Abraham Linsenmayer <abe.linsenmayer@gmail.com>
2024-01-13 15:51:03 -0600

    First commit

---
+++ src/main/kotlin/abe/owtt/OtherWorldsThanTheseDataGenerator.kt
@@ -0,0 +1,9 @@
+package abe.owtt
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object OtherWorldsThanTheseDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
