0ee4f2265fd9e5345d941dee174b598bc93ca061
Abraham Linsenmayer <abe.linsenmayer@gmail.com>
2024-01-14 15:32:33 -0600

    Changed modid to owtt

--- src/main/kotlin/abe/owtt/OtherWorldsThanThese.kt
+++ src/main/kotlin/abe/owtt/OtherWorldsThanThese.kt
@@ -1,15 +1,26 @@
 package abe.owtt
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
+const val MOD_ID = "owtt"
+
 object OtherWorldsThanThese : ModInitializer {
-    private val logger = LoggerFactory.getLogger("other_worlds")
+    private val logger = LoggerFactory.getLogger("owtt")
+
+	private val DEV_WAND = Item(FabricItemSettings().maxCount(1))
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		logger.info("Hello Fabric world!")
+
+		Registry.register(Registries.ITEM, Identifier(MOD_ID, "dev_wand"), DEV_WAND)
 	}
 }
